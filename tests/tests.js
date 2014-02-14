efa90e71bc67ec7616d42d5430c4ea8532762901
alu0100693096 <alu0100693096@ull.edu.es>
2014-02-14 19:21:28 +0000

    Añadido nuevo test

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,21 @@
 var assert = chai.assert;
 
 suite('csv', function() {
-    test('Entrada valida', function() {
+  
+  
+test('Creacion correcta de la tabla', function() {
+        
+original.value = '"producto", "precio" \n "camisa", "4,3"';
+
+calculate();
+        
+assert.equal(finaltable.className, "output");
+    
+});
+
+
+
+test('Entrada valida', function() {
         original.value = '"producto", "precio" \n "camisa", "4,3"';
 	calculate();
         assert.equal(finaltable.innerHTML, '\n\t<table class="center">\n\t  \n\t    \n\t    \t<tbody><tr>\n            \n\t      <td>producto</td>\n\t    \n\t      <td>precio</td>\n\t    \n\t    </tr>\n\t  \n\t    \n\t    \t<tr>\n            \n\t      <td>camisa</td>\n\t    \n\t      <td>4,3</td>\n\t    \n\t    </tr>\n\t  \n\t</tbody></table>\n    ');
